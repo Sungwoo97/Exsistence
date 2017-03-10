5d49ddf4a40a29d684fe4b3c48f2f6f1cb666a89
KimHyouksoo <gurtn08@naver.com>
2017-03-11 01:36:40 +0900

    세팅 UI추가 밝기 조절 슬라이더 추가

--- Exsistence/Assets/Script/scSceneMove.cs
+++ Exsistence/Assets/Script/scSceneMove.cs
@@ -5,30 +5,42 @@ using UnityEngine.SceneManagement;
 
 public class scSceneMove : MonoBehaviour
 {
-    //이동할 씬의 이름을 미리 받아옴
-    public string sceneName;
+    //누른 버튼의 이름을 미리 받아옴
+    public string buttonName;
 
     public void MoveScene()
     {
-        //씬의 이름을 아래 케이스들과 비교
-        switch (sceneName)
+        
+
+        //버튼의 이름을 아래 케이스들과 비교
+        switch (buttonName)
         {
-            //씬의 이름이 Main이거나 loadeGame이면 sceneName의 씬으로 이동
-            case "Main":
+            //버튼의 이름이 main이거나 loadeGame이면 buttonName의 씬으로 이동
+            case "main":
             case "loadGame":
-                SceneManager.LoadScene(sceneName);
+                SceneManager.LoadScene(buttonName);
                 break;
-            //씬의 이름이 leaveGame이면 게임 종료
+
+            //버튼의 이름이 leaveGame이면 게임 종료
             case "leaveGame":
                 //SceneManager의 게임 종료를 몰라서 구형식으로 작성함.
                 Application.Quit();
                 break;
+
+            //버튼의 이름이 setting이면 리소스 폴더에있는 UI를 가져와서 Instantiate함
             case "setting":
-                GameObject canvas = Resources.Load("Prefab/Canvas") as GameObject;
+                GameObject canvas = Resources.Load("Prefabs/SettingCanvas") as GameObject;
                 Instantiate(canvas, new Vector3(0,0,0), Quaternion.identity);
                 break;
+
+            //버튼의 이름이 Back이면 가져온 canvas 삭제
+            case "back":
+                Destroy(GameObject.Find("SettingCanvas(Clone)"));
+                break;
+
+            //아무 버튼도 아닐 시 에러 출력
             default:
-                Debug.Log("지정되지 않은 버튼 / Unspecified button.");
+                Debug.Log(buttonName + "은 지정되지 않은 버튼 / Unspecified button.");
                 break;
         }
     }
---
+++ Exsistence/Assets/Script/scSetting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scSetting : MonoBehaviour
+{
+    float ambientValue = 0f;
+    float soundValue = 0f;
+
+    private void OnGUI()
+    {
+        //x 스크린.X / 2 - 400, y 스크린.y / 2 + 250 위치에 가로, 세로 100, 50 인 슬라이더를 생성(최소 0, 최대 1);
+        ambientValue = GUI.HorizontalSlider(new Rect(Screen.width / 2 - 400, Screen.height / 2 + 250, 100, 50), ambientValue, 0f, 1.0f);
+        RenderSettings.ambientLight = new Color(ambientValue, ambientValue,ambientValue, 1);
+    }
+
+}
