c942ccca764f2986b05c2672aec09f394b4c88b3
SolarCcc <wooshwoo@naver.com>
2017-03-13 19:57:50 +0900

    신현

---
+++ Exsistence/Assets/Script/scEventFirst.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scEventFirst : MonoBehaviour , iEvent{
+
+    public scRunner Runner;
+	// Use this for initialization
+	void Start () {
+        Runner.SetEvent(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Run()
+    {
+        Debug.Log("이벤트 실행 됨");
+    }
+
+}
---
+++ Exsistence/Assets/Script/scRunner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scRunner : MonoBehaviour {
+
+    iEvent evt;
+    public int evtidx = 0;  //이벤트 인덱스 변수
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetEvent(iEvent ev)
+    {
+        evt = ev;
+    }
+
+    void OnCollisionEnter(Collider coll)    //충돌처리
+    {
+        Debug.Log("충돌하였음"); //충돌감지
+        if (true)
+        {
+            evt.Run();
+        }
+    }
+}
+
+public interface iEvent
+{
+    void Run();
+}
