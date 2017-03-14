19e7db4d080c484e82d7ae9f2fac1b1ce64912ec
Sungwoo97 <gwaja97@gmail.com>
2017-03-14 23:06:43 +0900

    상호작용

--- Exsistence/Assets/Script/SCobjHUD.cs
+++ Exsistence/Assets/Script/SCobjHUD.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class SCobjHUD : MonoBehaviour {
-    public TextMesh hudText;
-    public float objside;
-    public bool interaction = false;
-    private Transform interactionTr;
-    private Transform playerTr;
-    private MeshRenderer interactionMesh;
-    private CapsuleCollider playerRd;
-	// Use this for initialization
+    public TextMesh hudText;                // 텍스트
+    private float objside;                  // 거리 계산
+    public bool interaction = false;        // 상호작용 on/off
+    private Transform interactionTr;        // 상호작용 받는 것의 위치 값 받아오는 변수
+    private Transform playerTr;             // 플레이어의 위치 값을 받아오는 변수
+    private MeshRenderer interactionMesh;   // 상호작용이 되면 Meshrenderer on
+    private CapsuleCollider playerRd;       // collider radius 값과 거리를 같게 해줄 변수
+    public Transform target;
+    // Use this for initialization
 	void Start () {
         interactionTr = GameObject.Find("interaction").transform;
         playerTr = GameObject.Find("Capsule").transform;
@@ -19,6 +20,7 @@ public class SCobjHUD : MonoBehaviour {
         interactionMesh.enabled = false;
         playerRd = GameObject.Find("Capsule").GetComponent<CapsuleCollider>();
         objside = playerRd.radius;
+        
     }
 
     // Update is called once per frame
